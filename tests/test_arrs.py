18e95ee7a55b1dc6b6786b10b8a20f333d5faf5b
Ivan Skliaruk <cunanori@gmail.com>
2023-12-07 14:20:47 +0100

    tests fixed

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,16 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
-
+    assert arrs.get([2, 11, 9], 3, "test") == "test"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 3, 9], 1) == [2, 3, 3, 9]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 1, 3], 2) == [3]
+    assert arrs.my_slice([1, 1, 3, '2', 1], 3) == ['2', 1]
+    assert arrs.my_slice([1, 1, 3], 1) == [1, 3]
+    assert arrs.my_slice([1, 1, [2, 3], 3], 2, 3) == [[2, 3]]
+    assert arrs.my_slice([1, 1, [2, 0], 3], None, -1) == [1, 1, [2, 0]]
+    assert arrs.my_slice([1, 1, [2, 12], 3], 2, 3) == [[2, 12]]
